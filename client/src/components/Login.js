f5b2ddd53da0b7934e8db54bf1224dd6ee4b9898
LenWinkler <lenwinkler86@gmail.com>
2019-10-18 11:57:44 -0400

    built form and added axiosWithAuth file

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
 const Login = () => {
+
+  const [userName, setUserName] = useState('');
+  const [pass, setPass] = useState('');
+
+  const nameHandler = e => {
+    setUserName(e.target.value)
+  };
+
+  const passHandler = e => {
+    setPass(e.target.value)
+  };
+
+  const submitHandler = () => {
+    const credentials = {
+      username: userName,
+      password: pass
+    }
+
+  axios
+  .post('http://localhost:5000/api/login', credentials)
+  .then(res => console.log(res))
+  .catch(err => console.log('post error', err.response))
+  }
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={submitHandler}>
+        <input type="text" value={userName} onChange={nameHandler} placeholder="username" />
+        <input type="password" value={pass} onChange={passHandler} placeholder="password" />
+        <button>Login</button>
+      </form>
     </>
   );
 };
